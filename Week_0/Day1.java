add33a27eed100efe7c0899e6a0c3a908dffa1b4
fayeemajidul <mmookta1@binghamton.edu>
2022-07-12 11:50:35 -0400

    Challenge 4 Completed

--- Week_0/Day1.java
+++ Week_0/Day1.java
@@ -2,11 +2,24 @@ public class Day1{
     //Instance Field
     public Day1(){} //Constructor Class
     public static void main(String[] args){
-        printMegaBytesAndKiloBytes(2500);
-        printMegaBytesAndKiloBytes(-1024);
-        printMegaBytesAndKiloBytes(5000);
+        /** Challenge 2 Output */
+        // printMegaBytesAndKiloBytes(2500);
+        // printMegaBytesAndKiloBytes(-1024);
+        // printMegaBytesAndKiloBytes(5000);
+
+        /** Challenge 3 Output */
+        //System.out.println(shouldWakeUp(true, 44));
+
+        /** Challenge 4 Output */
+        //System.out.println(isLeapYear(2400));
+
+        /** Challenge 5 Output */
+
     }
     public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        /** CHALLENGE 2
+         * Converts Kilobytes to Megabytes
+         */
         if(kiloBytes < 0){
             System.out.println("Invalid Value");
         }else if(kiloBytes >= 0){
@@ -14,5 +27,37 @@ public class Day1{
             int remainingKB  = kiloBytes%1024;
             System.out.println(kiloBytes + " KB = " + megabytes + "MB and " + remainingKB + " KB.");
         }
+    }
+    public static boolean shouldWakeUp(boolean isBarking, int hourOfDay){
+        /** CHALLENGE 3 
+         * Function returns true if dog barks
+        */
+        if(isBarking){
+            isBarking = (hourOfDay < 8 || hourOfDay > 22)? true:false;
+            if(hourOfDay < 0 || hourOfDay > 23){
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isLeapYear(int year){
+        if(year >= 1 && year <= 9999){
+            if(year%4 == 0){
+                if(year%100==0){
+                    if(year%400==0){
+                        return true;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 }
