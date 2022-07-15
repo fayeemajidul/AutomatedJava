8ead3726cb5ae4838068090d1327f44f7d6c5eb5
fayeemajidul <mmookta1@binghamton.edu>
2022-07-15 12:09:44 -0400

    Palindrome

--- Week_0/Java0/Day4.java
+++ Week_0/Java0/Day4.java
@@ -2,11 +2,6 @@ public class Day4{
     //Instance Field
     public Day4(){}//Constructor class
     /** -- Extracting Place Values */
-    public static void main(String[] args){
-        //System.out.println(123%10);
-        //System.out.println(123%10);
-
-    }
     public static int sumDigits(int number){
         if(number <= 10){
             return -1;
@@ -23,5 +18,30 @@ public class Day4{
             //125 --> 125%10 --> (5)  Then 125/10 --> 12.5 the ".5 " is dropped.
             //Then the condition turns into (while (12 > 0)) and 12 % 10 = 2
         }
+        return sumOfDigits;
+    }
+    public static void isPalindrome(int number){
+        //Reversing the Order of the Number:
+        /** Number Entered 125
+         * int reverse number = 0
+         * int digit = (number%10) --> 5 put that in var
+         * reverseNum += digit;
+         * number/=10;
+         * reverseNum *= 10;
+         * 
+         * ()
+         */
+        int reverseNumber = 0;
+        while(number > 0){
+            int lastDigit = (number % 10);
+            reverseNumber += lastDigit;
+            reverseNumber *=10;
+            number /= 10;
+        }
+        reverseNumber = reverseNumber/10;
+        System.out.println(reverseNumber);
+    }
+    public static void main(String[] args){
+        isPalindrome(31737182);
     }
 }
