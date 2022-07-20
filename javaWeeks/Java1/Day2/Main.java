90db1d1f0dd92730801ebf31487bb24fc6806822
fayeemajidul <mmookta1@binghamton.edu>
2022-07-20 15:42:59 -0400

    Finished Person Challenge

--- javaWeeks/Java1/Day2/Main.java
+++ javaWeeks/Java1/Day2/Main.java
@@ -2,16 +2,29 @@ public class Main{
     // Instance Field:
     public Main(){} // Constructor Method:
     public static void main(String[] args){
-        SimpleCalculator calculator = new SimpleCalculator();
-        calculator.setFirstNumber(5.0);
-        calculator.setSecondNumber(4);
-        System.out.println("add = "+  calculator.getAdditionResult());
-        System.out.println("subtract = "+  calculator.getSubtractionResult());
-        calculator.setFirstNumber(5.25);
-        calculator.setSecondNumber(0);
-        System.out.println("multiply = "+  calculator.getMultiplicationResult());
-        System.out.println("Divide = " + calculator.getDivisionResult());
+        //Object 1: Calculator:
+        // SimpleCalculator calculator = new SimpleCalculator();
+        // calculator.setFirstNumber(5.0);
+        // calculator.setSecondNumber(4);
+        // System.out.println("add = "+  calculator.getAdditionResult());
+        // System.out.println("subtract = "+  calculator.getSubtractionResult());
+        // calculator.setFirstNumber(5.25);
+        // calculator.setSecondNumber(0);
+        // System.out.println("multiply = "+  calculator.getMultiplicationResult());
+        // System.out.println("Divide = " + calculator.getDivisionResult());
 
-
+        //Object 2: Person:
+        // Person fayeem = new Person();
+        // fayeem.setFirstName("Fayeem");
+        // fayeem.setLastName("Mooktadeer");
+        // Person Albert = new Person();
+        // Albert.setFirstName("");
+        // Albert.setLastName("");
+        // System.out.println(Albert.getFirstandLast());
+        // System.out.println(fayeem.getFirstandLast());
+        // fayeem.setAge(20);
+        // Albert.setAge(15);
+        // System.out.println(fayeem.isTeen());
+        // System.out.println(Albert.isTeen());
     }
 }
