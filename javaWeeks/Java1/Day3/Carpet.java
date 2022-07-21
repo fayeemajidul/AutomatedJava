1b4f1ef9b0f374b770a6f42f032fa503bdd7d756
fayeemajidul <mmookta1@binghamton.edu>
2022-07-21 12:48:19 -0400

    Multiple Classes

---
+++ javaWeeks/Java1/Day3/Carpet.java
@@ -0,0 +1,18 @@
+public class Carpet {
+    //Instance Field:
+    double cost;
+
+    //Constructor Method:
+    public Carpet(){
+        this(0);
+    }
+    public Carpet(double cost){
+        this.cost = cost;
+    }
+
+    //Accessor Methods:
+    public double getCost(){
+        return cost;
+    }
+
+}
