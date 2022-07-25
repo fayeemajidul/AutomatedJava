712162d44130f0a27a72b84e4d9099dd951ba800
fayeemajidul <mmookta1@binghamton.edu>
2022-07-25 14:01:10 -0400

    Project Poke

--- javaWeeks/Java2/Day0/PokemonProject/Pokemon.java
+++ javaWeeks/Java2/Day0/PokemonProject/Pokemon.java
@@ -2,14 +2,13 @@ import java.util.Random;
 import java.util.Scanner;
 public class Pokemon{
     //Instance Field:
-    String name; int health; String elementType; int strength; int chemistry;
+    static String name; int health; String elementType; int strength; int chemistry;
 
     //Constructor Method:
     public Pokemon(){
         this("Unnamed Pokemon", 0, "default element", 0, 0);
     }
     public Pokemon(String name, int health, String elementType, int chemistry, int strength ){
-        this.name = name;
         this.health = health;
         this.elementType = elementType;
         this.chemistry = chemistry;
@@ -22,10 +21,11 @@ public class Pokemon{
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please Choose the element for your Pokemon: Earth, Lightning, Fire, Water");
         this.elementType = scanner.nextLine();
+        elementType();
         for(int i = 0; i < 10; i++){
             System.out.print("-");
         }
-        System.out.println(" \n\t\t/ User Stats / \n" + "Pokemon Name: " + this.name);
+        System.out.println(" \n\t\t/ User Stats / \n" + "Pokemon Name: " + name);
         System.out.println("Health: " + this.health);
         System.out.println("Element Type: " + this.elementType);
         System.out.println("Chemistry: " + this.chemistry);
@@ -33,6 +33,12 @@ public class Pokemon{
             System.out.print("-");
         }
         scanner.close();
+    }
+    //Getter Function
+    public int arbitraryValue(){
+        Random rand = new Random();
+        int randomValue = rand.nextInt(0,100);
+        return randomValue;
     }
 
     //Accessors: Mutation Methods:
@@ -42,6 +48,23 @@ public class Pokemon{
         System.out.println(name + " default strength has been updated to " + strength);
     }
     public void elementType(){
-        
+        int randomValue = arbitraryValue();
+        switch(elementType.toLowerCase()){
+            case "lightning":
+            this.chemistry = randomValue;
+            break;
+
+            case "earth":
+            this.chemistry = randomValue;
+            break;
+
+            case "fire":
+            this.chemistry = randomValue;
+            break;
+
+            case "water":
+            this.chemistry = randomValue;
+            break;
+        } 
     }
 }
