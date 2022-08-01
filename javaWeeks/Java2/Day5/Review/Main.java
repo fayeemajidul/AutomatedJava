16576b227c263fc1d404041d9138835d47090a22
fayeemajidul <mmookta1@binghamton.edu>
2022-07-31 20:08:10 -0400

    please don't give up

---
+++ javaWeeks/Java2/Day5/Review/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    //Instance Field:
+    int counter = 0;
+    //Loops count from 1-10:
+
+    public Main(){} // Constructor Method
+    public static void main(String[] args) {
+        System.out.println("Testing the environment");
+    }
+}
