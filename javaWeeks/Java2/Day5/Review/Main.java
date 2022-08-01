870eb6a3eb1b3e8fb7288038eb40cd45f011eecc
fayeemajidul <mmookta1@binghamton.edu>
2022-07-31 20:49:43 -0400

    Reviewing Loops / Section 5

--- javaWeeks/Java2/Day5/Review/Main.java
+++ javaWeeks/Java2/Day5/Review/Main.java
@@ -1,10 +1,38 @@
 public class Main{
-    //Instance Field:
-    int counter = 0;
     //Loops count from 1-10:
-
+    public static int loopRecorder(){
+        //Do While Loop
+        int counter = 0;
+        boolean stop = false;
+        do{
+            counter++;
+            System.out.println(counter);
+            stop = (counter < 10)? true:false;
+        }while(stop == true);
+        return counter;
+    }
+    public static void whileLoops(int numEntered){
+        int sum = 0;
+        int numExtracted = 0;
+        while(numEntered > 0){
+            numExtracted = numEntered % 10;
+            numEntered/=10;
+            sum += numExtracted;
+            System.out.println(sum);
+        }
+                
+    }
+    public static void forLoops(){
+        for(int i = 1; i <= 10; i++){
+            System.out.println("Iteration number: " + i);
+        }
+    }
     public Main(){} // Constructor Method
     public static void main(String[] args) {
-        System.out.println("Testing the environment");
+        // System.out.println("Testing the environment");
+        // System.out.println(loopRecorder());
+        // whileLoops(124);
+        forLoops();
+
     }
 }
