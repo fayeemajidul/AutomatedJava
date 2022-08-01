58d347ec56cd085b4a0e05988f22b4e568e277ca
fayeemajidul <mmookta1@binghamton.edu>
2022-08-01 12:35:09 -0400

    health Status

--- javaWeeks/Java3/Day0/Pokemon/Attack.java
+++ javaWeeks/Java3/Day0/Pokemon/Attack.java
@@ -27,12 +27,21 @@ public class Attack{
     public void specialMove2(){
         int hunger = health.getHunger();
         hunger -= 20;
-        energy -=20;
+        energy -=25;
         double criticalDamage = energyFunction();
         energyFunction();
-        double strength = RandomFunction().nextInt(0,100) * criticalDamage;
+        double strength = RandomFunction().nextInt(20,100) * criticalDamage;
         System.out.println("Pokemon used Special Move 2, it had a damage of " + strength);
     }
+    public void specialMove3(){
+        int hunger = health.getHunger();
+        int healthBar = health.getHealth();
+        health.feedMove();
+        healthBar += 10;
+        energy +=20;
+        System.out.println("Pokemon used Special Move 3");
+    }
+
     public double energyFunction(){
         if(energy >= 85){
             //Create an attack critical bonus of 10%
@@ -50,4 +59,3 @@ public class Attack{
 //Finished Attack Function Just need to add a hunger component to increase Energy:
 //In order to do that I need to play with the Health function:
 //Work on health function after break health shoudl directly affect the energy function in this file
-//First create a hunger function on health to then implement on energy
