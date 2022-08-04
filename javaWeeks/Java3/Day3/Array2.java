0d4248dd457e85ab729ac565e08a570c7e523a6c
fayeemajidul <mmookta1@binghamton.edu>
2022-08-04 18:36:17 -0400

    Getting Logic

---
+++ javaWeeks/Java3/Day3/Array2.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array2 {
+    //Instance Field:
+    //Constructor Method:
+    public Array2(){}
+    public static int [] arrayEntered(int numEntered){
+        int [] arrayInput = new int [numEntered];
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Numbers Entered \r");
+        for(int i = 0; i < arrayInput.length; i++){
+            arrayInput[i] = scan.nextInt();
+        }
+
+        return arrayInput;
+    }
+    public static int [] reversedArray(int numEntered){
+
+        int [] arrayInput = arrayEntered(numEntered);
+        int [] array = Arrays.copyOf(arrayInput, arrayInput.length);
+        int MaxIndex = array.length - 1; // Takes the Last Element
+        int halfLength = (array.length) / 2; //Splits
+
+        //Now Traverse to the Sequence and Reverse the sequence:
+        for(int i = 0; i < halfLength; i++){
+            //Checks Each Element
+            int temp = array[i];
+            array[i] = array[MaxIndex- i];
+            array[MaxIndex - i] = temp;
+        }
+        return array;
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(reversedArray(4)));
+    }
+}
