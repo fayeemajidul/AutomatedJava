5af5aef9382bb2d6008304ae7aabb413866a6dd9
fayeemajidul <mmookta1@binghamton.edu>
2022-08-11 15:52:00 -0400

    Book Challenge

---
+++ javaWeeks/Java4/Day3/ArrayReview/Customer.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+//Within this class you should store, customers name, customers details information along with book preferences.
+//Each Customer should have a book list where you can get the book list and return it.
+
+//Create fucntions of FindBookList or edit booklist: Review arrays concepts:
+public class Customer {
+    //Instance Field:
+    private String name; private String bookPreference; private ArrayList <Books> bookList;
+    public Customer(String name, String bookPreference){
+        
+    }
+}
---
+++ javaWeeks/Java4/Day3/ArrayReview/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    //Instance Field
+    public Main(){}
+    public static void main(String[] args) {
+    Books bookList = new Books("Meditation", "Marcus Aralius", 3121);
+    System.out.println(bookList.toString());
+    }
+}
