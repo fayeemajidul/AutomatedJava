8fc145bd2a63d63d4c57958072145ee7b1a43141
fayeemajidul <mmookta1@binghamton.edu>
2022-08-17 17:21:59 -0400

    Finished Challenge

--- javaWeeks/Java5/Day1/Challenge/ListItem.java
+++ javaWeeks/Java5/Day1/Challenge/ListItem.java
@@ -20,23 +20,4 @@ public abstract class ListItem {
     public void setValue(Object value) {
         this.value = value;
     }
-    
-    // END:
-
 }
-
-
-
-// 1.  ListItem (abstract class)
-
-//     -  It has three protected fields. Two ListItems called rightLink and leftLink, and an Object called value.
-
-//     -  A constructor that takes an Object and initialises the field value with the parameter that was passed in.
-
-//     -  And seven methods:
-
-//         -  next(), setNext(), previous(), setPrevious() and compareTo() which are package-private and abstract (see child class for declaration).
-
-//         -  getValue(), takes no parameters and returns its value.
-
-//         -  setValue(), takes an Object and assigns it to value.
