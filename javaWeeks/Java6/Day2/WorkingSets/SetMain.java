507e36ac8c44ecb864cdf2e67ca75d1c43fb7da7
fayeemajidul <mmookta1@binghamton.edu>
2022-08-25 15:14:55 -0400

    Day 3

--- javaWeeks/Java6/Day2/WorkingSets/SetMain.java
+++ javaWeeks/Java6/Day2/WorkingSets/SetMain.java
@@ -51,15 +51,27 @@ public class SetMain{
         String [] divineWords = {"to", "err", "is", "human", "to","forgive", "divine"};
         nature.addAll(Arrays.asList(divineWords));
 
-        System.out.println("Nature - Divine:");
-        Set<String> diff1 = new HashSet<>(nature);
-        diff1.remove(nature);
-        printSet(diff1);
+        // System.out.println("Nature - Divine:");
+        // Set<String> diff1 = new HashSet<>(nature);
+        // diff1.remove(nature);
+        // printSet(diff1);
 
-        System.out.println("Divine - Nature:");
-        Set <String> diff2 = new HashSet<>(divine);
-        diff2.remove(divine);
-        printSet(diff2);
+        // System.out.println("Divine - Nature:");
+        // Set <String> diff2 = new HashSet<>(divine);
+        // diff2.remove(divine);
+        // printSet(diff2);
+
+        // Set <String> unionTest = new HashSet<>(nature);
+        // unionTest.addAll(divine);
+        // Set <String> intersectionTest = new HashSet<>(nature);
+        // intersectionTest.retainAll(divine);
+        // System.out.println("Symmetric Difference");
+        // unionTest.removeAll(intersectionTest);
+        // printSet(unionTest);
         
+        /* Review Bulk Operators
+         * Retain All
+         * Other Methods.
+         */
     }
 }
