92adbc164cf7430e41a7aa417b31087cc132f77c
fayeemajidul <mmookta1@binghamton.edu>
2022-08-31 14:11:52 -0400

    CssSelector

--- seleniumWeeks/selenium/Selenium0/Day1/selenium1/src/main/java/day1/App.java
+++ seleniumWeeks/selenium/Selenium0/Day1/selenium1/src/main/java/day1/App.java
@@ -1,5 +1,6 @@
 package day1;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 import org.openqa.selenium.By;
@@ -8,8 +9,25 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
- * Hello world!
+ * Types of Locators:
+ *ID, XPATH, CSS Selector, Name, Tag Name, Link Text, Partial Link Text
  */
+
+ //Syntax for Css Selector:
+ /* By (Class Name) -> tagname.classname 
+  * By (ID) -> tagname#id -> input#inputUsername
+
+  General Syntax:
+  *** 3: Tagname[attribute = 'value']
+  input[placeholder = 'Username']
+
+  //Implicit Waits: Tells you the amount of seconds to wait.
+    ** driver.manage().timeouts().implicitlyWait(Duration.ofSeconds());
+
+    To Check for Unique Elements on a webpage, you can do $('CssSelector') --> to check if the element is unique.
+
+ */
+
 public final class App implements WebDriver {
     private App() {
     }
@@ -17,9 +35,14 @@ public final class App implements WebDriver {
         //Setting up the System first.
         System.setProperty("webdriver.gecko.driver", "/Users/fayeemmooktadeer/Downloads/geckodriver" );
         WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.youtube.com/");
-        System.out.println(driver.hashCode());
-        driver.navigate();
+        driver.get("https://sso.teachable.com/secure/9521/identity/login/password");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        driver.findElement(By.id("email")).sendKeys("fayeemf@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("King1234!");
+        driver.findElement(By.className("btn-primary")).click();
+        System.out.println(driver.findElement(By.cssSelector("div.bodySmall")).getText());
+
+        //driver.quit();
 
 
     }
