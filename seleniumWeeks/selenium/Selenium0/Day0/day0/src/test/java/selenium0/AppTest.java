883959b6b5d04c1a85abe0609c5a9319fabc5830
fayeemajidul <mmookta1@binghamton.edu>
2022-08-30 21:31:21 -0400

    Reworking foundations

---
+++ seleniumWeeks/selenium/Selenium0/Day0/day0/src/test/java/selenium0/AppTest.java
@@ -0,0 +1,9 @@
+package selenium0;
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, Fayeem. Please cheer up !");
+    }
+}
