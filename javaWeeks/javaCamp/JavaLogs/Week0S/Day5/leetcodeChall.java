ae6f1cb23b19c7b25c32b75d9f510af2a2d53102
fayeemajidul <mmookta1@binghamton.edu>
2022-09-06 14:00:53 -0400

    roman numbers

--- javaWeeks/javaCamp/JavaLogs/Week0S/Day5/leetcodeChall.java
+++ javaWeeks/javaCamp/JavaLogs/Week0S/Day5/leetcodeChall.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class leetcodeChall{
 public leetcodeChall(){}
-
-public static int romanRules(int number){
-    /* We'll Create Rules */
+public static ArrayList <Integer> romanNumerals(){
     ArrayList <Integer> listOfRules = new ArrayList<Integer>();
     listOfRules.add(1);
     listOfRules.add(5);
@@ -13,12 +12,31 @@ public static int romanRules(int number){
     listOfRules.add(100);
     listOfRules.add(500);
     listOfRules.add(1000);
+    return listOfRules;
+}
 
-    //We want to return the number w/ the smallest remainder:
+public static int romanRules(int number){
+    /* Declarations:  */
+    ArrayList <Integer> listOfRules = romanNumerals();
     int smallest_Remainder = 0;
     int compareNumber;
+
     for(int i = 0; i < listOfRules.size(); i++){
-        compareNumber = (number)% listOfRules.get(i);
+        //46: 
+        if(listOfRules.get(i) % number != number){
+            while(number != 0){
+                number %=listOfRules.get(i);
+                compareNumber = number;
+                
+            }
+        }else{
+            //Not divisible by element:
+            System.out.println("Roman Numeral: " + listOfRules.get(i) + " not applicable");
+        }
+
+
+
+        compareNumber = (number) % listOfRules.get(i);
         if(compareNumber == number){
             System.out.println(number + " is not divisible by " + listOfRules.get(i));
         }else{
@@ -31,20 +49,36 @@ public static int romanRules(int number){
     }
     return smallest_Remainder;
 }
-
-// public static void romanRules(int number){
-//     while(number > 0){
-//         //Create a rule to get the largest roman:
-
-//     }
-// }
-
-
-
-
-
-
     public static void main(String[] args) {
         romanRules(120);
+    }
+}
+class Solution{
+    static HashMap <Character, Integer> charMap = new HashMap<Character, Integer>();
+
+    public static int convertRomanNumbertoInt(String str){
+        if(str == null || str.length() == 0){
+            return 0;
+        }
+        charMap.put('I', 1);
+        charMap.put('V', 5);
+        charMap.put('X', 10);
+        charMap.put('L', 50);
+        charMap.put('C', 100);
+        charMap.put('D', 500);
+        charMap.put('M', 1000);
+        // XI --> if 5 > 1 --> then do Add
+        int result = 0;
+        for(int i = 0; i < str.length() -1; i++){
+            if(charMap.get(str.charAt(i)) >= charMap.get(str.charAt(i+1))){
+                result += charMap.get(str.charAt(i));
+            }else{
+                result -= charMap.get(str.charAt(i));
+            }
+        }
+        
+    }
+    public static void main(String[] args) {
+        
     }
 }
