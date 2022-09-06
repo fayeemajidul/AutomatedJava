5041d1d13a851ac23eedcfd76a4b331aaa08288d
fayeemajidul <mmookta1@binghamton.edu>
2022-09-06 15:37:32 -0400

    Fixed code

--- javaWeeks/javaCamp/JavaLogs/Week0S/Day5/leetcodeChall.java
+++ javaWeeks/javaCamp/JavaLogs/Week0S/Day5/leetcodeChall.java
@@ -69,14 +69,17 @@ class Solution{
         charMap.put('M', 1000);
         // XI --> if 5 > 1 --> then do Add
         int result = 0;
+
         for(int i = 0; i < str.length() -1; i++){
             if(charMap.get(str.charAt(i)) >= charMap.get(str.charAt(i+1))){
                 result += charMap.get(str.charAt(i));
             }else{
                 result -= charMap.get(str.charAt(i));
             }
+            //Because the iteration doesn't traverse to the element we manaually traversed it:
+            result += charMap.get(str.charAt(str.length() -1));
         }
-        
+        return result;
     }
     public static void main(String[] args) {
         
