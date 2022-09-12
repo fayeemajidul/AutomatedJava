c0ad6c1298c41792f65b95d0dbfc692a7420e198
fayeemajidul <mmookta1@binghamton.edu>
2022-09-12 14:20:13 -0400

    Day 0 -- Editing

--- seleniumWeeks/selenium/Selenium1/Day1/Day0/src/test/java/daytest.java
+++ seleniumWeeks/selenium/Selenium1/Day1/Day0/src/test/java/daytest.java
@@ -1,7 +1,67 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class daytest {
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+
+// Practice with assertTrue, assertFalse, assertEquals = practice w/ assertions.
+public class daytest implements  WebDriver{
+    @Test
+    public static void main(String[] args){
+        System.setProperty("webdriver.gecko.driver", "/Users/fayeemmooktadeer/Downloads/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://spicejet.com");
+        Assert.assertFalse(driver.findElement(By.cssSelector("input[id*= 'SeniorCitizenDiscount']")).isSelected());
+
+    }
+    @Test
+    public static void seleniumAssignment(){
+        System.setProperty("webdriver.gecko.driver", "/Users/fayeemmooktadeer/Downloads/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("checkBoxOption1")).click();
+        Assert.assertTrue(driver.findElement(By.id("checkBoxOption1")).isSelected());
+        driver.findElement(By.id("checkBoxOption1")).click();
+        Assert.assertFalse(driver.findElement(By.id("checkBoxOption1")).isSelected());
+        /*Check for textBox*/
+        System.out.println(driver.findElements(By.cssSelector("input [type ='checkbox']")).size());
+
+    }
+
+    @Test
+    public static void calendarAutomation(){
+
+        System.setProperty("webdriver.gecko.driver", "/Users/fayeemmooktadeer/Downloads/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1200));
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.manage().window().maximize();
+        try{
+            Thread.sleep(2000);
+        }catch(Exception e){
+            System.out.println("Error");
+        }
+        driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).click();
+        WebElement element = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+        Select dropdown = new Select(element);
+        dropdown.selectByIndex(3);
+        System.out.println(dropdown.getFirstSelectedOption().getText());
+        driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+
+        //driver.quit();
+    }
+
+
+
 
     @BeforeClass
     public void setUp() {
@@ -18,4 +78,70 @@ public class daytest {
         System.out.println("Slow test");
     }
 
+
+    /* Abstract Methods, from interface*/
+    @Override
+    public void get(String url) {
+
+    }
+
+    @Override
+    public String getCurrentUrl() {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public List<WebElement> findElements(By by) {
+        return null;
+    }
+
+    @Override
+    public WebElement findElement(By by) {
+        return null;
+    }
+
+    @Override
+    public String getPageSource() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void quit() {
+
+    }
+
+    @Override
+    public Set<String> getWindowHandles() {
+        return null;
+    }
+
+    @Override
+    public String getWindowHandle() {
+        return null;
+    }
+
+    @Override
+    public TargetLocator switchTo() {
+        return null;
+    }
+
+    @Override
+    public Navigation navigate() {
+        return null;
+    }
+
+    @Override
+    public Options manage() {
+        return null;
+    }
 }
