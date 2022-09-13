763cfcf0b7d5491734b9108659c853f1d4169e1d
fayeemajidul <mmookta1@binghamton.edu>
2022-09-13 11:21:24 -0400

    IDEA configs

---
+++ seleniumWeeks/selenium/Selenium1/Day1/selenium1/src/test/java/testingDay1.java
@@ -0,0 +1,102 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Set;
+
+public class testingDay1 implements WebDriver {
+    public static void main(String[] args){
+        System.out.println("Not loved");
+    }
+    @BeforeClass
+    public void setUp() {
+        // code that will be invoked when this test is instantiated
+    }
+
+    @Test(groups = { "fast" })
+    public void aFastTest() {
+        System.out.println("Fast test");
+    }
+
+    @Test(groups = { "slow" })
+    public void aSlowTest() {
+        System.out.println("Slow test");
+    }
+    public static void practiceSignUp(){
+        System.setProperty("webdriver.gecko.driver", "/Users/fayeemmooktadeer/Downloads/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+        driver.manage().window().maximize();
+    }
+
+    /* Abstract WebDriver Methods: */
+
+    @Override
+    public void get(String url) {
+
+    }
+    @Override
+    public String getCurrentUrl() {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public List<WebElement> findElements(By by) {
+        return null;
+    }
+
+    @Override
+    public WebElement findElement(By by) {
+        return null;
+    }
+
+    @Override
+    public String getPageSource() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void quit() {
+
+    }
+
+    @Override
+    public Set<String> getWindowHandles() {
+        return null;
+    }
+
+    @Override
+    public String getWindowHandle() {
+        return null;
+    }
+
+    @Override
+    public TargetLocator switchTo() {
+        return null;
+    }
+
+    @Override
+    public Navigation navigate() {
+        return null;
+    }
+
+    @Override
+    public Options manage() {
+        return null;
+    }
+}
+
