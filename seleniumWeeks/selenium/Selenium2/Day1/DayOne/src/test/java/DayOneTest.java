c59791cfb7a2cf00839538fc41b17c8fc6d4b448
fayeemajidul <mmookta1@binghamton.edu>
2022-09-13 12:47:25 -0400

    Finished -- Updating

---
+++ seleniumWeeks/selenium/Selenium2/Day1/DayOne/src/test/java/DayOneTest.java
@@ -0,0 +1,6 @@
+public class DayOneTest {
+    public static void main(String[] args){
+        System.out.println("No Matter How Hard You Tried It All Went to Shit. Remember that. Remember that Feeling. That Feeling of how you were resented by so many of her family members.");
+
+    }
+}
