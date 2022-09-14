bcaa2419baebbe592996cd19ec70937858030d29
fayeemajidul <mmookta1@binghamton.edu>
2022-09-14 11:51:59 -0400

    Okay

---
+++ javaWeeks/javaCamp/JavaLogs/Week1S/Day3/ClimbingStairs.java
@@ -0,0 +1,9 @@
+public class ClimbingStairs{
+    //Instance FIeld:
+    public static void numberOfStepsPath(int numberOfStairs){
+
+    }
+    public static void main(String[] args){
+        System.out.println("Completeing Challenge");
+    }
+}
