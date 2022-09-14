540ce1bea6993eac3d7ecd2aa2d2667fa4ba4918
fayeemajidul <mmookta1@binghamton.edu>
2022-09-14 12:04:49 -0400

    failed Challenge

--- javaWeeks/javaCamp/JavaLogs/Week1S/Day3/ClimbingStairs.java
+++ javaWeeks/javaCamp/JavaLogs/Week1S/Day3/ClimbingStairs.java
@@ -1,9 +1,27 @@
 public class ClimbingStairs{
     //Instance FIeld:
     public static void numberOfStepsPath(int numberOfStairs){
+        int pathOne = 0;
+        int remainderOfSteps = (numberOfStairs%2); //Will give me the remaining amount of steps. -> 
 
+        for(int i = 0; i < numberOfStairs; i++){
+            if(numberOfStairs/1 == 0){
+                pathOne +=1;
+                break;
+            }else if(numberOfStairs%2 == 0){
+                pathOne +=1;
+            }else if (remainderOfSteps - 1 == 0){
+                pathOne +=1;
+            }else{
+                System.out.println("Impossible to go up these stairs");
+            }
+        }
+        System.out.println(pathOne);
     }
+
     public static void main(String[] args){
-        System.out.println("Completeing Challenge");
+        numberOfStepsPath(3121);
     }
 }
+
+//We're just looking for factors here.
