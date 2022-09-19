8950b5e164b1a579b9a4498c2defbb61a6900729
fayeemajidul <mmookta1@binghamton.edu>
2022-09-19 11:33:56 -0400

    leetcode Challenge

---
+++ javaWeeks/javaCamp/JavaLogs/Week2S/Day1/OrderInsert.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class OrderInsert{
+    public static void addToArray(ArrayList <Integer> accessOrder){
+        /** Purpose of this method
+         * Create random sequence of numbers into Array.
+         */
+        Random rand = new Random();
+        for(int i = 0; i < 4; i++){
+            accessOrder.add(rand.nextInt(0,100));
+        }    
+        System.out.println("unorder: " + accessOrder);
+    }
+    public static void reOrderArray( int target, ArrayList <Integer> accessOrder){
+        //Sorted Array:
+        addToArray(accessOrder);
+        Collections.sort(accessOrder);
+        System.out.println("order: " + accessOrder);
+        System.out.println(target + ":" + accessOrder.get(target));
+        
+
+
+    }
+    public static void main(String[] args) {
+        ArrayList <Integer> accessOrder = new ArrayList<Integer>();
+        reOrderArray(3, accessOrder);
+
+    }
+
+}
--- seleniumWeeks/selenium/Selenium3/Day0/DayZero/src/test/java/DayZero.java
+++ seleniumWeeks/selenium/Selenium3/Day0/DayZero/src/test/java/DayZero.java
@@ -7,9 +7,8 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-
-import java.sql.Driver;
 import java.time.Duration;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -17,6 +16,7 @@ public class DayZero implements WebDriver {
     public static void main(String[] args){
         System.out.println("Testing the environment");
     }
+
     @Test
     public static void amazonTrialPage(){
         System.setProperty("webdriver.gecko.driver", "/Users/fayeemmooktadeer/Downloads/geckodriver");
@@ -35,6 +35,18 @@ public class DayZero implements WebDriver {
         mouseAction.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.LEFT_SHIFT).sendKeys("hello").sendKeys(Keys.ENTER).build().perform();
         //How do I hit enter after that?
     }
+    public static void practicingWindowHandles(){
+        System.setProperty("webdriver.gecko.driver", "/Users/fayeemmooktadeer/Downloads/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://rahulshettyacademy.com/loginpagePractise/#");
+        driver.manage().window().maximize();
+        driver.findElement(By.cssSelector(".blinkingText")).click();
+
+        //Stores the Windows in the set.
+        Set<String> windows = driver.getWindowHandles();
+        
+
+    }
     @Test
     public static void amazonSignUpPage(){
         System.setProperty("webdriver.gecko.driver", "/Users/fayeemmooktadeer/Downloads/geckodriver");
@@ -47,11 +59,30 @@ public class DayZero implements WebDriver {
         driver.findElement(By.id("createAccountSubmit")).click();
         driver.findElement(By.id("ap_customer_name")).sendKeys("John Doe");
         driver.findElement(By.id("ap_email")).sendKeys("6463214391");
-        driver.findElement(By.id("ap_password")).sendKeys("Phobe123!");
-        driver.findElement(By.id("ap_password_check")).sendKeys("Phobe123!");
+        driver.findElement(By.id("ap_password")).sendKeys("Phone123!");
+        driver.findElement(By.id("ap_password_check")).sendKeys("Phone123!");
         driver.findElement(By.id("continue")).click();
     }
+    @Test
+    public static void testingWindows(){
+        System.setProperty("webdriver.gecko.driver", "/Users/fayeemmooktadeer/Downloads/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://rahulshettyacademy.com/loginpagePractise/#");
+        driver.manage().window().maximize();
+        driver.findElement(By.cssSelector(".blinkingText")).click();
+        WebDriverWait explicitWait = new WebDriverWait(driver, Duration.ofMillis(10000));
+        Set<String> windows = driver.getWindowHandles(); // grabs the present window handles: [Parent window is the one used by get, child is the one you are trying to invoke/ click]
+        Iterator <String> iterateWindow = windows.iterator();
+        //Grabs the parent window from the iterator method: --> iterator.next gets the 0 element. [0,1]
+        String parentWindow = iterateWindow.next();
+        String childWindow = iterateWindow.next();
+        driver.switchTo().window(childWindow);
 
+        //https://rahulshettyacademy.com/documents-request
+        //driver.quit();
+
+        //
+    }
     /* Import Abstract Methods: */
     @Override
     public void get(String url) {
@@ -60,57 +91,46 @@ public class DayZero implements WebDriver {
     public String getCurrentUrl() {
         return null;
     }
-
     @Override
     public String getTitle() {
         return null;
     }
-
     @Override
     public List<WebElement> findElements(By by) {
         return null;
     }
-
     @Override
     public WebElement findElement(By by) {
         return null;
     }
-
     @Override
     public String getPageSource() {
         return null;
     }
-
     @Override
     public void close() {
 
     }
-
     @Override
     public void quit() {
 
     }
-
     @Override
     public Set<String> getWindowHandles() {
         return null;
     }
-
     @Override
     public String getWindowHandle() {
         return null;
     }
-
     @Override
     public TargetLocator switchTo() {
         return null;
     }
-
     @Override
     public Navigation navigate() {
         return null;
     }
-
     @Override
     public Options manage() {
         return null;
