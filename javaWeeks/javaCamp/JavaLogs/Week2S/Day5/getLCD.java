1d7b2a1bca3e56f67f312fbb79d10d2ed517fcf8
fayeemajidul <mmookta1@binghamton.edu>
2022-09-26 00:04:47 -0400

    Complete Challenge

--- javaWeeks/javaCamp/JavaLogs/Week2S/Day5/getLCD.java
+++ javaWeeks/javaCamp/JavaLogs/Week2S/Day5/getLCD.java
@@ -1,5 +1,31 @@
+import java.util.*;
+
 public class getLCD{
+    public static ArrayList<Integer> addRandomNumbersIntoArray(ArrayList<Integer> arrayOfNums, int numOfElements){
+        Random rand = new Random();
+        for(int i = 0; i < numOfElements; i++){
+            arrayOfNums.add(rand.nextInt(0,100));
+        }
+        Collections.sort(arrayOfNums);
+        return arrayOfNums;
+    }
+    public static void getTheGCF(ArrayList<Integer> arrayOfNums){
+        ArrayList <Integer> sortedList = addRandomNumbersIntoArray(arrayOfNums, 6);
+        int getElementOfLast = sortedList.get(sortedList.size() -1);
+        int getFirstElement = sortedList.get(0);
+        System.out.println("First Element: " + getFirstElement + "\nLast Element: " + getElementOfLast);
+        // Now Create an LCD
+        int GCF = 0;
+        for(int i = 1; i <= getElementOfLast; i++){
+            if(getFirstElement % i == 0 && getElementOfLast % i == 0){
+                GCF = i;
+            }
+        }
+        System.out.println("GCF is: " + GCF);
+    }
     public static void main(String[] args){
-        System.out.println("Testing the environment");
+        ArrayList <Integer> arrayOfNums = new ArrayList<Integer>(); 
+        getTheGCF(arrayOfNums);
+
     }
 }
