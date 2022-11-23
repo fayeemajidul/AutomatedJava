1cc99e9b748922229695d30321f012181b369ac5
fayeemajidul <mmookta1@binghamton.edu>
2022-11-22 21:42:40 -0500

    Selenium Review

---
+++ seleniumWeeks/selenium/SeleniumReview/SeleniumReview/src/test/java/ReviewIntegration.java
@@ -0,0 +1,44 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class ReviewIntegration {
+    @Test
+    public static void pageStructure(){
+
+        //Overall Global stuff:
+        System.setProperty("webdriver.gecko.driver", "/Users/fayeemmooktadeer/Desktop/Grid/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://rahulshettyacademy.com/client");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        //Juicy Login Details: [DDT testing].
+        driver.findElement(By.id("userEmail")).click();
+        driver.findElement(By.id("userEmail")).sendKeys("fayeemajidul@gmail.com");
+        driver.findElement(By.id("userPassword")).click();
+        driver.findElement(By.id("userPassword")).sendKeys("Password123");
+        driver.findElement(By.id("login")).click();
+        WebDriverWait explicitWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        explicitWait.until(ExpectedConditions.titleIs("Let's Shop"));
+
+        //Now that we are on shopping page, we want to grab one item and add it to the cart.
+        List <WebElement> items = driver.findElements(By.cssSelector(".mb-3"));
+        for(WebElement item : items){
+            String formattedText = item.getText().split(" ")[0].trim();
+            if(formattedText.equalsIgnoreCase("ZARA")){
+                item.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+            }
+        }
+        explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("#toast-container")));
+        driver.findElement(By.xpath("//button[@routerlink='/dashboard/cart']")).click();
+
+    }
+}
