61d91f1a8d3ec423bda68b6db520e99b6851d62e
fayeemajidul <mmookta1@binghamton.edu>
2022-12-04 23:49:39 -0500

    JIRA API

--- restWeeks/rest/rest0/day1/DynamicAPI/src/main/java/org/example/DynamicUtilization.java
+++ restWeeks/rest/rest0/day1/DynamicAPI/src/main/java/org/example/DynamicUtilization.java
@@ -3,12 +3,15 @@ package org.example;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 
+import io.restassured.filter.session.SessionFilter;
 import io.restassured.path.json.JsonPath;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DynamicUtilization {
     RestAssured restAssured = new RestAssured();
+    static String uniqueId;
+    static SessionFilter session = new SessionFilter();
     @Test(dataProvider = "bookData")
     public static void dynamicJSON(String name, String isbn, String aisle, String author){
         String responseApi = RestAssured.baseURI = "http://216.10.245.166";
@@ -24,5 +27,28 @@ public class DynamicUtilization {
     @DataProvider(name = "bookData")
     public Object[][] getData(){
         return new Object[][]{{"Harry Potter", "13412", "ABC", "J.K Rowling"},{"12 Rules of Life", "41441", "cd41", "Jordan Peterson"},{"Lady Midnight", "41309", "412z", "Cassandra Clarke"}};
+    }
+
+    @Test
+    public static void restJiraAuthentication(){
+        baseURI = "http://localhost:8080";
+        String jiraUniqueID = given().header("Content-Type", "application/json").body("{ \"username\": \"fayeemtests\", \"password\": \"Fayeem123456!\" }").log().all().filter(session).when().post("/rest/auth/1/session").then().extract().response().asString();
+
+        JsonPath extractUniqueID = new JsonPath(jiraUniqueID);
+        uniqueId = extractUniqueID.getString("session.value");
+    }
+
+    @Test
+    public static void addCommentToJira(){
+        restJiraAuthentication();
+        baseURI = "http://localhost:8080";
+        given().log().all().pathParam("key", "10101").header("content-type","application/json").body("""
+                {
+                    "body": "This is my first comment that only administrators can see.",
+                    "visibility": {
+                        "type": "role",
+                        "value": "Administrators"
+                    }
+                }""").filter(session).when().post("/rest/api/2/issue/{key}/comment").then().log().all().assertThat().statusCode(201);
     }
 }
