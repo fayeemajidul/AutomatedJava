594b860202d7b9f7df8d4f2d21be3caef1ea562d
fayeemajidul <mmookta1@binghamton.edu>
2022-12-09 23:13:33 -0500

    RestReview

--- restWeeks/rest/rest0/day3/RestPractice/src/main/java/org/example/RestTest.java
+++ restWeeks/rest/rest0/day3/RestPractice/src/main/java/org/example/RestTest.java
@@ -6,6 +6,7 @@ import static io.restassured.RestAssured.*;
 
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -41,6 +42,34 @@ public class RestTest {
         Header putNewHeader = new Header("Preference","Women");
 
         RestAssured.given().header("Preference","Women");
+    }
+    @Test
+    public static void jsonResponse(){
+        baseURI = "https://my-json-server.typicode.com";
 
+        Response response2 = when().get( baseURI+ "/typicode/demo/posts").then().log().all().extract().response();
+
+        Headers headersForApp = response2.getHeaders();
+
+        //Asserts that the there is a header of that name
+        for(Header singleHeader : headersForApp){
+            String headerName = singleHeader.getName();
+            String headerValue = singleHeader.getValue();
+
+            Assert.assertEquals(response2.getHeader(headerName), headerValue);
+        }
+        //Grab the Json:
+        String JsonResponseFile = when().get( baseURI+ "/typicode/demo/posts").then().extract().body().asString();
+        JsonPath grabJson = new JsonPath(JsonResponseFile);
+
+        System.out.println(JsonResponseFile);
+        String ID = grabJson.getString("[0].title");
+        System.out.println(ID);
+
+        int elementList = grabJson.getInt("grabJson.size()");
+        for(int i = 0; i < elementList; i++){
+            String titleOfJson = grabJson.getString("[" + i + "].title");
+            System.out.println(titleOfJson);
+        }
     }
 }
