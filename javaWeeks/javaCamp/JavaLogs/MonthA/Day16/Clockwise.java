1157dac5fca312d5c2213fcf7537e6a587e32129
fayeemajidul <mmookta1@binghamton.edu>
2023-01-27 13:50:03 -0500

    LeetCode Medium

---
+++ javaWeeks/javaCamp/JavaLogs/MonthA/Day16/Clockwise.java
@@ -0,0 +1,8 @@
+public class Clockwise{
+    public static void main(String[] args){
+        System.out.println("testing the environment");
+        for(int i = 0; i < 4; i++){
+            System.out.println("Testing the environment");
+        }
+    }
+}
