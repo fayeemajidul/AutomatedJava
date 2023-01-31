11196d219234d068f2d99ad87957439a1a0d61a2
fayeemajidul <mmookta1@binghamton.edu>
2023-01-31 12:54:35 -0500

    Autonomy

---
+++ javaWeeks/javaCamp/JavaLogs/MonthA/Day17/HealthCareDomain/src/main/java/org/example/Main.java
@@ -0,0 +1,92 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+
+public class Main implements WebDriver {
+    @Test
+    public static void healthCareDomain() throws InterruptedException {
+        System.setProperty("webdriver.gecko.driver", "/Users/fayeemmooktadeer/Desktop/Grid/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://healthfirst.org/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        //Click navigations:
+        driver.findElement(By.className("ml-4")).click();
+        Thread.sleep(3000);
+
+    }
+
+    @Override
+    public void get(String url) {
+
+    }
+
+    @Override
+    public String getCurrentUrl() {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public List<WebElement> findElements(By by) {
+        return null;
+    }
+
+    @Override
+    public WebElement findElement(By by) {
+        return null;
+    }
+
+    @Override
+    public String getPageSource() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void quit() {
+
+    }
+
+    @Override
+    public Set<String> getWindowHandles() {
+        return null;
+    }
+
+    @Override
+    public String getWindowHandle() {
+        return null;
+    }
+
+    @Override
+    public TargetLocator switchTo() {
+        return null;
+    }
+
+    @Override
+    public Navigation navigate() {
+        return null;
+    }
+
+    @Override
+    public Options manage() {
+        return null;
+    }
+}
