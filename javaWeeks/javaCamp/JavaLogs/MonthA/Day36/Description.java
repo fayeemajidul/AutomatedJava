cf90fcceb16115315899d7dd9665c646d6e57189
fayeemajidul <mmookta1@binghamton.edu>
2023-03-23 10:34:25 -0400

    Learning Automation Pipelines

--- javaWeeks/javaCamp/JavaLogs/MonthA/Day36/Description.java
+++ javaWeeks/javaCamp/JavaLogs/MonthA/Day36/Description.java
@@ -1,5 +1,6 @@
 public class Description{
     public static void main(String[] args){
         System.out.println("Getting into the flow of things");
+        System.out.println("CI/CD pipelines");
     }
 }
