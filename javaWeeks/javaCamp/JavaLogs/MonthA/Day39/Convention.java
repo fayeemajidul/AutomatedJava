9c5a34efef247895dc8724588b31e5b34212ec4d
fayeemajidul <mmookta1@binghamton.edu>
2023-03-29 00:58:10 -0400

    Loops

---
+++ javaWeeks/javaCamp/JavaLogs/MonthA/Day39/Convention.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class Convention{
+    public static void main(String[] args){
+        System.out.println("Naming conventions");
+        createRandomString();
+    }
+    public static void createRandomString(){
+        Random rand = new Random();
+        // System.out.println("Testing the environment + your age is : " + age);
+        int age = 0;
+
+        while(age != 21){
+            System.out.println(age + "is not your age");
+            age++;
+            if(age == 21){
+                break;
+            }
+        }
+    }
+}
