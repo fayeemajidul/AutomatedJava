601a1568367172679263f756a6d684347f49c704
fayeemajidul <mmookta1@binghamton.edu>
2023-04-10 03:16:50 -0400

    Commit

--- javaWeeks/javaCamp/JavaLogs/MonthA/Day40/Trial.java
+++ javaWeeks/javaCamp/JavaLogs/MonthA/Day40/Trial.java
@@ -1,5 +1,7 @@
 public class Trial{
     public static void main(String[] args){
         System.out.println("testing the environment");
+        System.out.println("testing the environment");
+
     }
 }
