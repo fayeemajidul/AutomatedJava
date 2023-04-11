2afc0d05ad36227a87d3136dd6b7d2627e9e6e3f
fayeemajidul <mmookta1@binghamton.edu>
2023-04-11 03:17:19 -0400

    Trial

--- javaWeeks/javaCamp/JavaLogs/MonthA/Day40/Trial.java
+++ javaWeeks/javaCamp/JavaLogs/MonthA/Day40/Trial.java
@@ -2,6 +2,7 @@ public class Trial{
     public static void main(String[] args){
         System.out.println("testing the environment");
         System.out.println("testing the environment");
+        System.out.println("testing the environment");
 
     }
 }
